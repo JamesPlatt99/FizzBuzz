0f029c5fe1bc3a7ae27c499dd788ad7c84388309
James Platt <james.platt@neptune.com>
2017-10-03 14:26:59 +0100

    The one liner challenge is now solved in one line. The user can choose which version of the program to run. Added exception handling to user input for the one liner.

--- FizzBuzz/ConsoleApp1/Program.cs
+++ FizzBuzz/ConsoleApp1/Program.cs
@@ -11,23 +11,52 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            while (true) { 
+                Console.WriteLine("Fizz Buzz");
+                Console.WriteLine("   1. IEnumerable interface challenge");
+                Console.WriteLine("   2. One Liner challenge");
+                String input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        IEnumerableChallenge(args);
+                        break;
+                    case "2":
+                        OneLiner();
+                        break;
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }            
+        }
+
+        private static void IEnumerableChallenge(string[] args)
+        {
             processor fizzBuzzer = new processor();
             fizzBuzzer.Start(args);
 
-            foreach(var value in fizzBuzzer)
+            foreach (var value in fizzBuzzer)
             {
                 Console.WriteLine(value);
             }
-
-            Console.WriteLine();
-            Console.WriteLine();
-
-            IEnumerable<string> fizzBuzz = Enumerable.Range(1, 100).Select(n => (n % 15 == 0) ? "FizzBuzz" : (n % 3 == 0) ? "Fizz" : (n % 5 == 0) ? "Buzz" : n.ToString()).ToList();
-            foreach(string output in fizzBuzz)
-            {
-                Console.WriteLine(output);
+        }
+        private static void OneLiner()
+        {
+            Boolean validInput = false;
+            while (!validInput) { 
+                Console.Write("Please enter your max value: ");
+                try
+                {
+                    validInput = true;
+                    //Here's the one line that it needs
+                    Enumerable.Range(1, Convert.ToInt32(Console.ReadLine())).Select(n => (n % 15 == 0) ? "FizzBuzz" : (n % 3 == 0) ? "Fizz" : (n % 5 == 0) ? "Buzz" : n.ToString()).ToList().ForEach(Console.WriteLine);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, please try again.");
+                    validInput = false;
+                }
             }
-            Console.ReadLine();
         }
     }
 }
