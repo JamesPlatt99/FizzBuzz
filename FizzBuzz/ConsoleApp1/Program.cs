f78be19f38e391d9453c8e5943bb93b59948ee9c
James Platt <james.platt@neptune.com>
2017-10-04 09:51:44 +0100

    Added a new option for the one liner v2 allowing the user to create a new banging rap song.

--- FizzBuzz/ConsoleApp1/Program.cs
+++ FizzBuzz/ConsoleApp1/Program.cs
@@ -65,9 +65,29 @@ namespace ConsoleApp1
         public static void OneLinerV2()
         {
             Dictionary<int,String> rules = new Dictionary<int,string>();
+            Console.WriteLine("Please choose a ruleset: ");
+            Console.WriteLine(" 1. Write me a wicked new rap song");
+            Console.WriteLine(" 2. Normal");
+            if (Console.ReadLine() == "1")
+            {
+                rules.Add(72, "Bang");
+                rules.Add(8, "Boom");
+                rules.Add(9, "Zoom");
+                rules.Add(10, "Pow");
+                rules.Add(11, "Sha'taang");
+                rules.Add(12, "Nyyuuuuum");
+                rules.Add(13, "KAPLAAAAAR");
+                rules.Add(14, "PopPop");
+                rules.Add(15, "Bing");
+                rules.Add(16, "Shiddy");
+                rules.Add(2, "Bong");
+                rules.Add(4, "JustSauce");
+                rules.Add(7, "DaTingGoes");
+                rules.Add(6, "NoKetchup");
+                rules.Add(1, "Skkrrrrat");
+            }
             rules.Add(3, "Fizz");
             rules.Add(5, "Buzz");
-            rules.Add(7, "Bang");
             String x;
 
             Boolean validInput = false;
@@ -77,16 +97,18 @@ namespace ConsoleApp1
                     Console.Write("Please enter your max value: ");
                     
                     validInput = true;
-                    Enumerable.Range(1, Convert.ToInt32(Console.ReadLine())).Select(n => (x = String.Join("" , rules.Where( a => n % a.Key == 0).Select(b=> b.Value))) == "" ? n.ToString() : x).ToList().ForEach(Console.WriteLine);
+                    //Enumerable.Range(1, Convert.ToInt32(Console.ReadLine())).Select(n => (x = String.Join("" , rules.Where( a => n % a.Key == 0).Select(b=> b.Value))) == "" ? n.ToString() : x).ToList().ForEach(Console.WriteLine);
+                    Enumerable.Range(1, Convert.ToInt32(Console.ReadLine())).Select(
+                        i => Tuple.Create(i, string.Join("", rules.Where(rule => i % rule.Key == 0).Select(rule => rule.Value))))
+                        .Select(p => p.Item2 == "" ? p.Item1.ToString() : p.Item2)
+                        .ToList()
+                        .ForEach(Console.WriteLine);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
                     validInput = false;
                 }
-
-
-
             }
         }
         
